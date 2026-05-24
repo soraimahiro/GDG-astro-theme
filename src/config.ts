da5cf4b12bdb7ecd53df6545385efc4aa440e077
soraimahiro <cherngci96@gmail.com>
2026-05-24 23:10:26 +0800

    add footer cong into config.ts. By the way I change author to gdgoc.

--- src/config.ts
+++ src/config.ts
@@ -7,12 +7,47 @@ export interface AuthorConfig {
     media?: Media[];
 }
 
+export interface FooterLink {
+    name: string;
+    url: string;
+}
+
+export interface FooterSection {
+    title: string;
+    links: FooterLink[];
+}
+
+export interface FooterConfig {
+    copyrightName: string;
+    sections: FooterSection[];
+}
+
+// set your config here
 export const authorConfig: AuthorConfig = {
-    name: "Lion",
-    avatar: "/Logo.png",
-    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    name: "GDGoC NTNU",
+    avatar: "/GDG.jpg",
+    bio: "Welcome. We are GDG on Campus NTNU.",
     media: [
-        { iconName: "mingcute:github-fill", link: "https://github.com" },
-        { iconName: "mingcute:mail-fill", link: "mailto:example@example.com" }
+        { iconName: "mingcute:instagram-fill", link: "https://www.instagram.com/gdg.ntnu" },
+        { iconName: "mingcute:discord-fill", link: "https://discord.gg/ANfAvvrGZN" }
+    ]
+};
+
+export const footerConfig: FooterConfig = {
+    copyrightName: "GDG NTNU",
+    sections: [
+        {
+            title: "Links",
+            links: [
+                { name: "Github", url: "https://github.com/soraimahiro/Astro-Theme-Lion" }
+            ],
+        },
+        {
+            title: "Reference",
+            links: [
+                { name: "Astro", url: "https://astro.build" },
+                { name: "Tailwind CSS", url: "https://tailwindcss.com" }
+            ],
+        },
     ]
 };
